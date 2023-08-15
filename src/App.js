8ba4d3e2e3af7b1431e5d0177e7d6ff6a17cdb43
akaAditya <mymail.adityapatel@gmail.com>
2023-08-15 20:05:19 +0530

    second commit loader implement

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import "./App.css";
 
 function App() {
   const [movie, setMovie] = useState([]);
+  const [isLoader, setIsLoader] = useState(false);
   async function fetchMoviesLists() {
+    setIsLoader(true);
     const moviesURL = await fetch("https://swapi.py4e.com/api/films");
     const data = await moviesURL.json();
     const transformedData = data.results.map((movieData) => {
@@ -17,6 +19,7 @@ function App() {
       };
     });
     setMovie(transformedData);
+    setIsLoader(false);
   }
 
   return (
@@ -25,7 +28,9 @@ function App() {
         <button onClick={fetchMoviesLists}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movie} />
+        {!isLoader && movie.length > 0 && <MoviesList movies={movie} />}
+        {!isLoader && movie.length === 0 && <p>No Movie Found...</p>}
+        {isLoader && <p>Loading...</p>}
       </section>
     </React.Fragment>
   );
